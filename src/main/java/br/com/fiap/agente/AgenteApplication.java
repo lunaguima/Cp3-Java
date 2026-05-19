71c79c1f05ff865c93d371dcfc18bcf8561dc018
lunaguima <luna.dcarvalho05@gmail.com>
2026-05-19 10:50:35 -0300

    first commit - setup inicial da API

---
+++ src/main/java/br/com/fiap/agente/AgenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.agente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/agente/AgenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.agente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
